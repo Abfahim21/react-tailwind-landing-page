a660050bd7e064bce6eee1c0539c0f51828f7b06
Md. Abrar Fahim <abrarfahim21@gmail.com>
2023-07-19 06:45:41 +0600

    almost done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -16,22 +16,12 @@ export default function Header() {
                 <AiOutlineClose onClick={()=>setToggle(!toggle)} className='text-white text-3xl md:hidden block'/>
             }
             <ul className='hidden md:flex text-white gap-10'>
-            <li>
-                Home
-            </li>
-            <li>
-                Company
-            </li>
-            <li>
-                About
-            </li>
-            <li>
-                Resources
-            </li>
-            <li>
-                Contact
-            </li>
-        </ul>
+                <li className='cursor-pointer'>Home</li>
+                <li className='cursor-pointer'>Company</li>
+                <li className='cursor-pointer'>About</li>
+                <li className='cursor-pointer'>Resources</li>
+                <li className='cursor-pointer'>Contact</li>
+            </ul>
         {/* Responsive Menu */}
         <ul className={`duration-300 md:hidden w-full h-screen text-white fixed bg-black top-[98px] ${toggle ?'left-[-100%]' : 'left-[0px]'} `}>
             <li className='p-5'>
